894dbdfeacdf614ef8a1c23f082e44ce095002cd
Julio Machado <juliocarlos00@hotmail.com>
2021-05-13 16:52:06 -0300

    add/ home page + clear page + auth context

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import { FormEvent, useContext, useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+import styles from '../styles/home.module.css';
+
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn } = useAuth();
+
+  async function handleSubmit(event: FormEvent) {
+
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    }
+
+    await signIn(data);
+  }
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  )
+}
