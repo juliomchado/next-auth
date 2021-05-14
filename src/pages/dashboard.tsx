47fdbb312a575dee3864602b3597a3df8a00e746
Julio Machado <juliocarlos00@hotmail.com>
2021-05-14 16:45:11 -0300

    add/ auth validate

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useAuth } from "../context/AuthContext"
-import { api } from "../services/api";
+import { setupApiClient } from "../services/api";
+import { api } from "../services/apiClient";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
 
@@ -10,9 +12,20 @@ export default function Dashboard() {
         api.get('/me')
             .then(response => console.log(response))
             .catch(err => console.log(err))
-    }, [])
+    }, []);
 
     return (
         <h1>Dashboard: {user?.email}</h1>
     )
-}
+};
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+    const apiClient = setupApiClient(ctx);
+    const response = await apiClient.get('/me');
+
+    console.log(response.data)
+
+    return {
+        props: {}
+    }
+});
