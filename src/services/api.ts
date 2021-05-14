ef18509153274d6e5589b38d65c34976aec604d9
Julio Machado <juliocarlos00@hotmail.com>
2021-05-14 13:56:20 -0300

    add/ automatic logout

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
+import { signOut } from '../context/AuthContext';
 
 
 let cookies = parseCookies();
@@ -69,7 +70,9 @@ api.interceptors.response.use(response => {
             });
 
         } else {
-            // deslogar o usuário
+            signOut();
         }
     }
+
+    return Promise.reject(error);
 })
